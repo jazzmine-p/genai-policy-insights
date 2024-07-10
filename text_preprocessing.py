4aff0576fc7da2310379ab97169c6f4ab88eac07
Jasmine Pham <jazzmine@bu.edu>
2024-07-10 11:04:48 -0400

    Reorder text preprocessing

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -1,52 +1,53 @@
 import nltk
 from nltk import word_tokenize          
 from nltk.stem import WordNetLemmatizer 
+import pandas as pd
+import yaml
 import spacy
 import re
 import logging
+from constants import log_dir
 
 logger = logging.getLogger(__name__)
-
-# Delete unwanted sections
-def filter_sections(all_sections):
-    logger.info("Filtering sections")
-    
-    unwanted_sections_header = ["references", "appendix", "glossary", 'table of contents', 'acknowledgments', 'disclosure statement', 'author', 'contact', 'executive summary', 'introduction']
-    unwanted_sections = [section for section in all_sections if any(text in re.search(r'#{1,6} (.*)', section).group(1)  for text in unwanted_sections_header)]
-    filtered_sections = [section for section in all_sections if section not in unwanted_sections]
-
-    return filtered_sections
-
+with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+entity_labels = config['entity_labels']
 
 nlp = spacy.load("en_core_web_sm")
+
 def remove_names(text):
     doc = nlp(text)
-    entities = [ent.text for ent in doc.ents if ent.label_ in ["PERSON", "ORG", "DATE"]]
 
-    logging.info(f"Entities: {entities}")
+    ent_dict = {ent.text: ent.label_ for ent in doc.ents if ent.label_ in entity_labels}
+    entities = ent_dict.keys()
 
     for entity in entities:
         text = text.replace(entity, "")
-    return text
+    return text, ent_dict
 
 # Text preprocessing
-def preprocess_text(all_sections):
+def preprocess_text(docs):
     logger.info("Preprocessing text")
 
     preprocessed_sections = []
+    all_entities = []
 
-    phrase_mapping = {
-    "genai": "generative AI",
-    "gen ai": "generative AI",
-    "generative ai": "generative AI",
-    "generative artificial intelligence": "generative AI",
-    }
-
-    for text in all_sections:
+    for text in docs:
+        # Remove Markdown-style links
+        processed_text = re.sub(r'\[([^\]]*)\]\([^)]*\)', r'\1', text)
+        # Remove standalone URLs (http/https)
+        processed_text = re.sub(r'\b(?:https?://)\S+\b', '', processed_text)
+        # Remove URLs starting with www.
+        processed_text = re.sub(r'\b(?:www\.)\S+\b', '', processed_text)
+        # Consolidate phrases
+        phrase_mapping = config['phrase_mapping']
+        for key, value in phrase_mapping.items():
+            processed_text = re.sub(r'\b' + re.escape(key) + r'\b', value, processed_text, flags=re.IGNORECASE)
         # Remove names
-        text = remove_names(text)
+        processed_text, ent = remove_names(processed_text)
+        all_entities.extend(ent.items())
         # Remove numbers
-        processed_text = re.sub(r'\d+', '', text)
+        processed_text = re.sub(r'\d+', '', processed_text)
         # Remove "et al"
         processed_text = re.sub(r'et al', '', processed_text)
         # Remove page divider
@@ -54,17 +55,14 @@ def preprocess_text(all_sections):
         # Normalize white spaces
         processed_text = re.sub(r'\s+', ' ', processed_text)
         # Remove non-word characters and white space
-        #processed_text = re.sub(r'\W', ' ', processed_text)
-        # Remove Markdown-style links
-        processed_text = re.sub(r'\[([^\]]*)\]\([^)]*\)', r'\1', processed_text)
-        # Remove standalone URLs (http/https)
-        processed_text = re.sub(r'\b(?:https?://)\S+\b', '', processed_text)
-        # Remove URLs starting with www.
-        processed_text = re.sub(r'\b(?:www\.)\S+\b', '', processed_text)
-        # Consolidate phrases
-        for key, value in phrase_mapping.items():
-            processed_text = re.sub(r'\b' + re.escape(key) + r'\b', value, processed_text, flags=re.IGNORECASE)
+        processed_text = re.sub(r'\W', ' ', processed_text)
 
         preprocessed_sections.append(processed_text)
+
+        # Convert the accumulated entity data to a DataFrame
+        ent_df = pd.DataFrame(all_entities, columns=['Entity', 'Label'])
+    
+        # Save the DataFrame to a CSV file
+        ent_df.to_csv(f'{log_dir}/removed_entities.csv', index=False)
 
     return preprocessed_sections
