dc17255200a94d3bd15238f5c9d0a389619d91a5
Jasmine Pham <jazzmine@bu.edu>
2024-07-10 11:00:27 -0400

    Update additional NLP text splitting

--- data_loader.py
+++ data_loader.py
@@ -3,8 +3,14 @@ import fitz
 import pymupdf4llm
 import re
 import logging
+import yaml
+import json
+from langchain_text_splitters import NLTKTextSplitter
 
 logger = logging.getLogger(__name__)
+
+with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
 
 # Convert PDFs to Markdown
 def convert_pdfs_to_markdown(directory):
@@ -18,10 +24,19 @@ def convert_pdfs_to_markdown(directory):
         with open(markdown_file, "w", encoding="utf-8") as output:
             output.write(md_text)
 
+# Delete unwanted sections
+def filter_sections(all_sections):
+    logger.info("Filtering sections")
+    
+    unwanted_sections = [section for section in all_sections if any(text in re.search(r'#{1,6} (.*)', section, re.IGNORECASE).group(1)  for text in config['unwanted_sections_header'])]
+    filtered_sections = [section for section in all_sections if section not in unwanted_sections]
+
+    return filtered_sections
+
 # Split markdown text by section
 def split_markdown_by_section(markdown_text):
     # Lowercase the markdown text to make the regex case-insensitive
-    #markdown_text = markdown_text.lower()
+    markdown_text = markdown_text.lower()
 
     # Delete figures and images
     markdown_text = re.sub(r'!\[.*?\]\(.*?\)', '', markdown_text)
@@ -39,12 +54,21 @@ def split_markdown_by_section(markdown_text):
 def save_sections_to_list(directory):
     sections_list = []
     markdown_files = [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith('.md')]
-    
+    token_splitter = NLTKTextSplitter(chunk_size=3000)
+
     for markdown_file in markdown_files:
-        with open(markdown_file, 'r', encoding='utf-8') as file:
-            markdown_text = file.read()
-            sections = split_markdown_by_section(markdown_text)
-            sections_list.extend(sections)
+        try:
+            with open(markdown_file, 'r', encoding='utf-8') as file:
+                markdown_text = file.read()
+                sections = split_markdown_by_section(markdown_text)
+                filtered_sections = filter_sections(sections)
+                
+                for section in filtered_sections:
+                    chunks = token_splitter.split_text(section)
+                    sections_list.extend(chunks)
+                    
+        except Exception as e:
+            logging.error(f"Error processing file {markdown_file}: {e}")
     
     return sections_list
 
@@ -58,5 +82,8 @@ def data_loader_subfolders(main_directory):
         #convert_pdfs_to_markdown(subfolder)
         sections = save_sections_to_list(subfolder)
         all_sections.extend(sections)
+    
+    with open("documents.json", "w") as file:
+        json.dump(all_sections, file)
     
     return all_sections
